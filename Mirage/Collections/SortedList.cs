56d363ab4ae82eb54e8d88f7209fef0cbc993fe7
Viswanath Ramanan <viswanr@microsoft.com>
2017-11-20 12:08:33 -0800

    Add tests for SortedList. Fix some custom comparer issues

--- Mirage/Collections/SortedList.cs
+++ Mirage/Collections/SortedList.cs
@@ -1,61 +1,95 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Mirage.Collections
 {
+    /// <summary>
+    /// Represents a list that is always sorted
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public sealed class SortedList<T> : ICollection<T>
     {
-        private readonly List<T> list;
+        private readonly List<T> list = new List<T>();
         private readonly IComparer<T> comparer;
+        /// <summary>
+        /// Create a sorted list using the default comparer
+        /// </summary>
+        public SortedList()
+        {
 
+        }
+        /// <summary>
+        /// Create a new sorted list with the given comparer
+        /// </summary>
+        /// <param name="comparer">Comparer to compare items of the list</param>
         public SortedList(IComparer<T> comparer)
         {
-            this.list = new List<T>();
+            if (comparer == null)
+            {
+                throw new ArgumentNullException(nameof(comparer));
+            }
+
             this.comparer = comparer;
         }
-
+        /// <summary>
+        /// Add a new item to the list
+        /// </summary>
+        /// <param name="item"></param>
         public void Add(T item)
         {
-            int index = list.BinarySearch(item);
+            int index = (comparer == null) ? list.BinarySearch(item) : list.BinarySearch(item, comparer);
             if (index < 0)
                 index = -(index + 1);
             list.Insert(index, item);
         }
-
+        /// <summary>
+        /// Clear all the items in the list
+        /// </summary>
         public void Clear()
         {
             this.list.Clear();
         }
-
+        /// <summary>
+        /// Gets whether an item is contained in the list
+        /// </summary>
+        /// <param name="item">Item to look for</param>
+        /// <returns></returns>
         public bool Contains(T item)
         {
-            return (this.list.BinarySearch(item) >= 0);
+            return comparer == null ? (this.list.BinarySearch(item) >= 0) : (this.list.BinarySearch(item, comparer) >= 0);
         }
-
+        /// <summary>
+        /// Copy all items from the list into an array
+        /// </summary>
+        /// <param name="array">Destination array to copy to</param>
+        /// <param name="arrayIndex">Starting index into the array</param>
         public void CopyTo(T[] array, int arrayIndex)
         {
             this.list.CopyTo(array, arrayIndex);
         }
-
+        /// <summary>
+        /// Gets the number of items in the list
+        /// </summary>
         public int Count
         {
             get { return this.list.Count; }
         }
-
+        /// <summary>
+        /// Gets the index of an item in the list
+        /// Returns -1 if the item is not found in the list
+        /// </summary>
+        /// <param name="item">Item to find the index of</param>
+        /// <returns></returns>
         public int IndexOf(T item)
         {
-            int index = this.list.BinarySearch(item);
-            if (index < 0)
-                return -1;
-            else
-            {
-                while (index > 0 && (this.comparer.Compare(this.list[index - 1], item) == 0))
-                {
-                    index--;
-                }
-                return index;
-            }
+            int index = comparer == null ? this.list.BinarySearch(item) : this.list.BinarySearch(item, comparer);
+            return (index >= 0) ? index : -1;
         }
-
+        /// <summary>
+        /// Array style accessor to get an item using the index
+        /// </summary>
+        /// <param name="index">Index of the item</param>
+        /// <returns>List item at the given index</returns>
         public T this[int index]
         {
             get
@@ -63,15 +97,21 @@ namespace Mirage.Collections
                 return this.list[index];
             }
         }
-
+        /// <summary>
+        /// Gets whether the list is readonly
+        /// </summary>
         public bool IsReadOnly
         {
             get { return false; }
         }
-
+        /// <summary>
+        /// Remove an item from the list
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public bool Remove(T item)
         {
-            int index = this.list.BinarySearch(item);
+            int index = comparer == null ? this.list.BinarySearch(item) : this.list.BinarySearch(item, comparer);
             if (index >= 0)
             {
                 this.list.RemoveAt(index);
@@ -80,12 +120,18 @@ namespace Mirage.Collections
             else
                 return false;
         }
-
+        /// <summary>
+        /// Gets an emumerator for the list
+        /// </summary>
+        /// <returns></returns>
         public IEnumerator<T> GetEnumerator()
         {
             return this.list.GetEnumerator();
         }
-
+        /// <summary>
+        /// Gets an enumerator for the list
+        /// </summary>
+        /// <returns></returns>
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
         {
             return this.list.GetEnumerator();
---
+++ Mirage_Test/Collections/SortedListTest.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Mirage.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace Mirage_Test.Collections
+{
+    [TestClass]
+    public class SortedListTest
+    {
+        [TestMethod]
+        public void Constructor_Validate_Null_Comparer()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new SortedList<int>(null));
+        }
+
+        [TestMethod]
+        public void Constructor_Default()
+        {
+            var list = new SortedList<int>();
+            Assert.IsNotNull(list);
+            Assert.AreEqual(list.Count, 0);
+            Assert.IsFalse(list.IsReadOnly);
+        }
+
+        [TestMethod]
+        public void Constructor_Valid_Comparer()
+        {
+            var list = new SortedList<int>(Comparer<int>.Default);
+            Assert.IsNotNull(list);
+            Assert.AreEqual(list.Count, 0);
+        }
+
+        [TestMethod]
+        public void Ensure_Sorting()
+        {
+            var list = new SortedList<int>();
+            list.Add(5);
+            list.Add(2);
+            list.Add(26);
+            list.Add(-17);
+            list.Add(18);
+
+            Assert.AreEqual(list.Count, 5);
+            Assert.AreEqual(list[0], -17);
+            Assert.AreEqual(list[1], 2);
+            Assert.AreEqual(list[2], 5);
+            Assert.AreEqual(list[3], 18);
+            Assert.AreEqual(list[4], 26);
+        }
+
+        [TestMethod]
+        public void Validate_Clear()
+        {
+            var list = new SortedList<int>();
+            list.Add(20);
+            list.Add(16);
+            list.Add(18);
+            Assert.AreEqual(list.Count, 3);
+
+            list.Clear();
+            Assert.AreEqual(list.Count, 0);
+        }
+
+        [TestMethod]
+        public void Validate_Contains()
+        {
+            var list = new SortedList<int>();
+            list.Add(85);
+            list.Add(-16);
+            list.Add(77);
+            list.Add(285);
+            Assert.AreEqual(list.Count, 4);
+
+            Assert.IsTrue(list.Contains(-16));
+            Assert.IsTrue(list.Contains(285));
+            Assert.IsFalse(list.Contains(284));
+        }
+
+        [TestMethod]
+        public void Validate_Copy_To()
+        {
+            var list = new SortedList<int>();
+            list.Add(85);
+            list.Add(-16);
+            list.Add(77);
+            list.Add(285);
+
+            var listArr = new int[5];
+            list.CopyTo(listArr, 1);
+
+            Assert.AreEqual(listArr[1], -16);
+            Assert.AreEqual(listArr[2], 77);
+            Assert.AreEqual(listArr[3], 85);
+            Assert.AreEqual(listArr[4], 285);
+        }
+
+        [TestMethod]
+        public void Validate_IndexOf()
+        {
+            var list = new SortedList<int>();
+            list.Add(18);
+            list.Add(-88);
+            list.Add(17);
+            Assert.AreEqual(list.Count, 3);
+
+            Assert.AreEqual(list.IndexOf(-88), 0);
+            Assert.AreEqual(list.IndexOf(18), 2);
+            Assert.AreEqual(list.IndexOf(16), -1);
+        }
+
+        [TestMethod]
+        public void Validate_Array_Accessor()
+        {
+            var list = new SortedList<int>();
+            list.Add(18);
+            list.Add(-88);
+            list.Add(17);
+            Assert.AreEqual(list.Count, 3);
+
+            Assert.AreEqual(list[0], -88);
+            Assert.AreEqual(list[2], 18);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => list[3]);
+        }
+
+        [TestMethod]
+        public void Validate_Remove()
+        {
+            var list = new SortedList<int>();
+            list.Add(18);
+            list.Add(-88);
+            list.Add(17);
+            Assert.AreEqual(list.Count, 3);
+            Assert.IsTrue(list.Contains(17));
+            Assert.AreEqual(list[1], 17);
+            Assert.AreEqual(list.IndexOf(17), 1);
+
+            list.Remove(17);
+            Assert.AreEqual(list.Count, 2);
+            Assert.IsFalse(list.Contains(17));
+            Assert.AreEqual(list.IndexOf(17), -1);
+            Assert.AreEqual(list[1], 18);
+        }
+
+        [TestMethod]
+        public void Validate_Custom_Comparer()
+        {
+            var list = new SortedList<int>(new CustomIntComparer());
+            Assert.IsNotNull(list);
+
+            list.Add(16);
+            list.Add(-24);
+            list.Add(8);
+            Assert.AreEqual(list.Count, 3);
+
+            // Validate in reverse order
+            Assert.AreEqual(list[0], 16);
+            Assert.AreEqual(list[1], 8);
+            Assert.AreEqual(list[2], -24);
+        }
+    }
+
+    sealed class CustomIntComparer : Comparer<int>
+    {
+        public override int Compare(int x, int y)
+        {
+            return x == y ? 0 : (x < y ? 1 : -1);
+        }
+    }
+}
